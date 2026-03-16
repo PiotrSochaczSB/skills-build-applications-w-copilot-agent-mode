a8c3a1ca1ee83a90930674b94aaae86d9fde4c2c
PiotrSochaczSB <Piotr.Sochacz@sainsburys.co.uk>
2026-03-16 11:22:11 +0000

    Refactor settings and urls: restrict ALLOWED_HOSTS and add api_root endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-v7h%*8nv*a!-_s%xi3!dl-yla!g_be#7$pdd_tgk3=*(g0whab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME', None)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_name:
+    ALLOWED_HOSTS.append(f'{codespace_name}-8000.app.github.dev')
 
 
 # Application definition
